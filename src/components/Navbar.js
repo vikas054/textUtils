078ee4125ce88604b0970419701714b937bad209
Vikas Chauhan <vikas_11013732@sipowers.com>
2023-07-27 08:03:17 +0530

    changes for deployment

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ export default function Navbar(props){
     return(
         <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
         <div className="container-fluid">
-          <a className="navbar-brand" href="/">{props.tittle}</a>
+          <a className="navbar-brand" href="#">{props.tittle}</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
@@ -15,11 +15,11 @@ export default function Navbar(props){
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
                 {/* <Link className="nav-link active" aria-current="page" to="/">Home</Link> */}
-                <a className="nav-link active" aria-current="page" href="/">Home</a>
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
               </li>
               <li className="nav-item">
                 {/* <Link className="nav-link" to="/about">{props.aboutText}</Link> */}
-                <a className="nav-link" href="/">{props.aboutText}</a>
+                <a className="nav-link" href="#">{props.aboutText}</a>
               </li>
           
               {/* <li className="nav-item">
